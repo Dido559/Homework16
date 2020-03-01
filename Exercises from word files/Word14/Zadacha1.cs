aa24b1e87f2a1a997fede1a3e51be3c337e29335
Dido559 <61014230+Dido559@users.noreply.github.com>
2020-03-01 12:39:17 +0200

    Adding exercises from Word 14!

---
+++ Exercises from word files/Word14/Zadacha1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> spisak = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                spisak.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int index = 0; index < spisak.Count; index++)
+            {
+                Console.WriteLine("list[{0}] = {1}", index, spisak[index]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises from word files/Word14/Zadacha2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.Write($" {numbers[i]} ");
+                } 
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
