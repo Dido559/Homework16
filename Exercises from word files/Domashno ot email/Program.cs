005411576be7f185a0bcd9d2c434ecbb153401e7
Dido559 <61014230+Dido559@users.noreply.github.com>
2020-03-23 21:52:14 +0200

    Commiting!

---
+++ Exercises from word files/Domashno ot email/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashna_rabota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string didi = Console.ReadLine();
+            if(didi.Length < 7)
+            {
+                Console.WriteLine("The string is too small! ;(");
+
+            }
+            else
+            {
+                int length = didi.Length;
+              
+                Console.WriteLine($"Without first 3 letters: {didi.Substring(3)}");
+                Console.WriteLine($"Without last 3 letters: {didi.Substring(0, length - 3)}");
+                Console.WriteLine($"Without first and last 3 letters {didi.Substring(3, length - 6)}");
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
