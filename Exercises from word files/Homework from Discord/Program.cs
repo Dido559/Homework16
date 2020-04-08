dfdc9525a16cdbfda2ebf3573d6b5337d62d76a9
Dido559 <61014230+Dido559@users.noreply.github.com>
2020-04-08 20:43:10 +0300

    Commiting!

---
+++ Exercises from word files/Homework from Discord/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistancionnoDomashno2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+            while (true)
+            {
+                var input = Console.ReadLine().Split(' ');
+                string comm = input[0].ToLower();
+                switch (comm)
+                {
+                    case "a":
+                        {
+                            phonebook.Add(input[1], input[2]);
+                            break;
+
+                        }
+                    case "s":
+                        {
+                           if (phonebook.ContainsKey(input[1]))
+                           {
+                                Console.WriteLine($"Name: {input[1]} | Number: {phonebook[input[1]]}");
+                           }
+                            else
+                            {
+                                Console.WriteLine($"The contact you wanted to view doesn't exist!");
+                            }
+                            break;
+                        }
+                    case "end":
+                        {
+                            return;
+                        }
+                }
+                
+
+
+
+
+            }
+        }
+    }
+}
